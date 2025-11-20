e20905888b9114fbbd7f1854a0310387f49e860f
maussn <naber.maurits@gmail.com>
2025-11-20 15:24:22 +0100

    remove tailwind related stuff

--- rotom.client/src/components/NavButton.tsx
+++ rotom.client/src/components/NavButton.tsx
@@ -1,4 +1,3 @@
-import classNames from "classnames";
 import { type To, useNavigate } from "react-router-dom";
 
 type Props = {
@@ -7,15 +6,10 @@ type Props = {
     isActive: boolean;
 };
 export const NavButton = (props: Props) => {
-    const { to, text, isActive } = props;
+    const { to, text } = props;
     const navigate = useNavigate();
 
-    return (<button className={classNames(
-        "py-1 px-3 rounded-full text-xl border-4",
-        "hover:text-neutral-800 ", "hover:bg-neutral-50", "hover:border-neutral-50 duration-300",
-        { "text-neutral-300 bg-sogyo border-neutral-300": !isActive },
-        { "text-neutral-800 bg-neutral-50 border-neutral-50": isActive })}
-        onClick={() => navigate(to)}>
+    return (<button onClick={() => navigate(to)}>
         {text}
     </button>)
 }
