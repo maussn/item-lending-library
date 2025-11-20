f50be0111914d05019eaa0a720bea22112f3faf1
maussn <naber.maurits@gmail.com>
2025-11-20 14:21:59 +0100

    add a fked up nav bar that goes nowhere

---
+++ rotom.client/src/router.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import { RootLayout } from "./layout/RootLayout";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    
+  }
+])
