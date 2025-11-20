b941555af235fbfead91c9f73ff6a9314a1a9a88
maussn <naber.maurits@gmail.com>
2025-11-20 09:28:04 +0100

    reorder overrides so correct profile is used

--- rotom.persistence/src/main/scala/nl/sogyo/persistence/H2Databases.scala
+++ rotom.persistence/src/main/scala/nl/sogyo/persistence/H2Databases.scala
@@ -3,10 +3,10 @@ package nl.sogyo.persistence
 import slick.lifted.TableQuery
 
 object H2AccountsDatabase extends AccountsDatabase with Tables {
+  override val profile = slick.jdbc.H2Profile
   override val table = TableQuery[AccountsTable]
   override val db = profile.api.Database.forConfig("H2-test")
-  override val profile = slick.jdbc.H2Profile
-
+  
 }
 
 object H2DatabaseProvider extends DatabaseProvider {
--- rotom.persistence/src/main/scala/nl/sogyo/persistence/MySQLDatabases.scala
+++ rotom.persistence/src/main/scala/nl/sogyo/persistence/MySQLDatabases.scala
@@ -5,13 +5,13 @@ import slick.lifted.TableQuery
 
 
 object MySQLAccountsDatabase extends AccountsDatabase with Tables {
+  override val profile = slick.jdbc.MySQLProfile
   override val table = TableQuery[AccountsTable]
   override val db = profile.api.Database.forConfig("rotom-root")
-  override val profile = slick.jdbc.MySQLProfile
+
 }
 
 object MySQLDatabaseProvider extends DatabaseProvider {
-
   override val accountsDatabase = MySQLAccountsDatabase
 
 }
