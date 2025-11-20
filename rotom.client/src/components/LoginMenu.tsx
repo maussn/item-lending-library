4db71d56826cca364a3613b56293bc9a62d091d2
maussn <naber.maurits@gmail.com>
2025-11-20 16:25:26 +0100

    fix logout

--- rotom.client/src/components/LoginMenu.tsx
+++ rotom.client/src/components/LoginMenu.tsx
@@ -1,16 +1,23 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { NavButton } from "./NavButton";
 import { useAuth } from "../contexts/AuthContext";
 
 export const LoginMenu = () => {
-  const { isLoggedIn, username, logout } = useAuth();
-  const { pathname } = useLocation();
+  const { isLoggedIn, username, logout } = useAuth()
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logout()
+    navigate("/")
+  }
+
   return (
     <div className="login-menu">
       {isLoggedIn ? (
         <div className="user-info">
-          <span className="username">{username}</span>
-          <button className="logout-button" onClick={logout}>Logout</button>
+          <span className="username" style={{padding: "1rem"}}>{username}</span>
+          <button className="logout-button" onClick={handleLogout}>Logout</button>
         </div>
       ) : (
         <NavButton to="/login" text="login" isActive={pathname === "/login"} />
