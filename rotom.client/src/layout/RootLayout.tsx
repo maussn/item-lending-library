753b44b3f6446fce69086f4a60e6ce590fa656d7
maussn <naber.maurits@gmail.com>
2025-11-20 14:42:23 +0100

    add non-functional nav-bar

--- rotom.client/src/layout/RootLayout.tsx
+++ rotom.client/src/layout/RootLayout.tsx
@@ -3,18 +3,19 @@ import logo from "../assets/0479Rotom-Mow.png";
 import { NavButton } from "../components/NavButton";
 
 export const RootLayout = () => {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-    return <div className="flex flex-col h-screen ">
-        <header className="bg-sogyo shadow-lg flex flex-row p-4">
-            <img src={logo} className="logo" />
-            <nav className="pt-4 flex-1 flex flex-row justify-center gap-2">
-                <NavButton to="/" text="Play" isActive={pathname === "/"} />
-                <NavButton to="/about" text="About" isActive={pathname === "/about"} />
-            </nav>
-        </header>
-        <main className="flex-1">
-            <Outlet />
-        </main>
-    </div>
+  return <div>
+    <header id="logo-header">
+      <img src={logo}/>
+      <nav>
+        <NavButton to="/" text="catalog" isActive={pathname === "/"} />
+        <NavButton to="/login" text="login" isActive={pathname === "/login"}/>
+      </nav>
+    </header>
+
+    <main className="flex-1">
+      <Outlet />
+    </main>
+  </div>
 };
